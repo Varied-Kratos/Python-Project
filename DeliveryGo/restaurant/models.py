f852bd0e2bf668a58d796871d02708f1f4dd72c3
da1lyre <aidgowork@gmail.com>
2025-12-22 22:57:10 +0300

    Update models.py

--- DeliveryGo/restaurant/models.py
+++ DeliveryGo/restaurant/models.py
@@ -19,7 +19,6 @@ class Category(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            from django.utils.text import slugify
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
 

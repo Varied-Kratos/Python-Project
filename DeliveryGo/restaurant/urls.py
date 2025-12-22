f45acd54e32ee813a9f01b7a0da119aae8d9013d
da1lyre <aidgowork@gmail.com>
2025-12-22 21:49:15 +0300

    Update urls.py

--- DeliveryGo/restaurant/urls.py
+++ DeliveryGo/restaurant/urls.py
@@ -7,23 +7,19 @@ urlpatterns = [
     path('menu/', views.menu, name='menu'),
     path('category/<int:category_id>/', views.category_menu, name='category_menu'),
 
-    # Корзина - только для авторизованных пользователей
     path('cart/', login_required(views.cart_view), name='cart'),
     path('cart/add/', login_required(views.add_to_cart), name='add_to_cart'),
     path('cart/update/<int:item_id>/', login_required(views.update_cart_item), name='update_cart_item'),
     path('cart/remove/<int:item_id>/', login_required(views.remove_cart_item), name='remove_cart_item'),
 
-    # Оформление заказа - только для авторизованных пользователей
     path('checkout/', login_required(views.checkout), name='checkout'),
     path('checkout/create_order/', login_required(views.create_order), name='create_order'),
     path('order-confirmation/<int:order_id>/', login_required(views.order_confirmation), name='order_confirmation'),
 
-    # API - некоторые эндпоинты требуют авторизации
     path('api/cart_count/', views.get_cart_count, name='cart_count'),
     path('api/cart_data/', login_required(views.get_cart_data), name='cart_data'),
     path('api/add_to_cart/', login_required(views.add_to_cart), name='add_to_cart_api'),
 
-    # API создания заказа - только для авторизованных пользователей
     path('api/create_order/', login_required(views.create_order), name='api_create_order'),
 
     path('about/', views.about, name='about'),
